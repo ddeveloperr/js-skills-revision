bb818843abf1ff71217fb1a8ef1f1d08c18fc841
KemalC <ddeveloperr@gmail.com>
2016-01-13 17:05:57 +0000

    Add area fn

---
+++ 2_Getting-functional/1_woof.js
@@ -0,0 +1,29 @@
+// IFY code with a lot of redudant code!
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+console.log(dogName + " says WOOF WOOF");
+} else {
+console.log(dogName + " says woof woof");
+}
+dogName = "spot";
+dogWeight = 13;
+if (dogWeight > 20) {
+console.log(dogName + " says WOOF WOOF");
+} else {
+console.log(dogName + " says woof woof");
+}
+dogName = "spike";
+dogWeight = 53;
+if (dogWeight > 20) {
+console.log(dogName + " says WOOF WOOF");
+} else {
+console.log(dogName + " says woof woof");
+}
+dogName = "lady";
+dogWeight = 17;
+if (dogWeight > 20) {
+console.log(dogName + " says WOOF WOOF");
+} else {
+console.log(dogName + " says woof woof");
+}
