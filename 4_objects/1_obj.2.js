0a8ac0e9633b373b53e792bcec635c4c8f9fd7c9
KemalC <ddeveloperr@gmail.com>
2016-01-16 17:20:19 +0000

    Add more obj and use it in example 2

--- 4_objects/1_obj.2.js
+++ 4_objects/1_obj.2.js
@@ -7,19 +7,60 @@
     convertible: false,
     mileage: 10001
  };
+ 
+ //..Second object
+ 
+ var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+ };
+
+// Third object
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+ };
+
+
+// Fourth object 
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
+
 
  function prequal(car) {
-   if (car.mileage > 10000) {
-     return false;
-   } else if (car.year > 2000) {
-   return false;
+    if (car.mileage > 10000) {
+        return false;
+   }    else if (car.year > 2000) {
+            return false;
   }
- return true;
+        return true;
  }
  
-var worthALook = prequal(taxi);
-  if (worthALook) {
-    console.log("You gotta check out this " + taxi.make + " " + taxi.model);
+ 
+ // test it with those are objects and see results
+ 
+var worthALook = prequal(fiat);
+    if (worthALook) {
+        console.log("You gotta check out this " + fiat.make + " " + fiat.model);
   } else {
-      console.log("You should really pass on the " + taxi.make + " " + taxi.model);
+        console.log("You should really pass on the " + fiat.make + " " + fiat.model);
   }
