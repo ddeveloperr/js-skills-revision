4a8d00f9c84959f3c03f355a2662fc940d503919
KemalC <ddeveloperr@gmail.com>
2016-01-18 22:24:56 +0000

    Typof example

---
+++ 5_types/1_types.js
@@ -0,0 +1,22 @@
+// 
+
+console.log(typeof test1);var test1 = "qwerty";
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6;
+var test7 = {"qwerty": 123};
+var test8 = ["qwerty", 123];
+function test9(){return "qwerty"}
+
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
